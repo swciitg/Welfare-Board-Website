eab205a50004623e5266426cc271266a24f4d794
daksh1223 <70289051+daksh1223@users.noreply.github.com>
2021-10-23 02:02:52 +0530

    Add necessary APIs

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const apiController = require('../controllers/api')
+
+router.route('/clubs').get(async (req, res) => {
+  let club_names = await apiController.get_all_clubs()
+  res.json(club_names)
+})
+router.route('/club/:club').get(async (req, res) => {
+  let club_details = await apiController.get_club_details(req.params.club)
+  res.json(club_details)
+})
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 const userRouter = require("./users");
+const apiRouter = require("./api");
 
 const indexController = require("../controllers/index");
 
 /* GET CONTROLLER */
+
 router.get("/", indexController.indexView);
 
 //= ==================================== DECLARE ALL YOUR ROUTERS HERE ==================================
 
+router.use("/api",apiRouter);
 router.use("/users", userRouter);
+
 module.exports = router;
